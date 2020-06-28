e6ec91195a2793823f8f949772962b3e806d8188
suryan535 <suryan535@gmail.com>
2020-06-28 15:37:39 +0530

    Updates

---
+++ precache-manifest.92935ca6ade0f2e6a186153dacbcab7a.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68965e418a532ad46e2e3770da848414",
+    "url": "/NumoUnoApp/index.html"
+  },
+  {
+    "revision": "e9df95308c4bb5fb808c",
+    "url": "/NumoUnoApp/static/css/main.3d0913f7.chunk.css"
+  },
+  {
+    "revision": "add3c5222cdc4bc3f49c",
+    "url": "/NumoUnoApp/static/js/2.19083fc9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/NumoUnoApp/static/js/2.19083fc9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9df95308c4bb5fb808c",
+    "url": "/NumoUnoApp/static/js/main.c0d81591.chunk.js"
+  },
+  {
+    "revision": "6e3ce093ca8d496846d5",
+    "url": "/NumoUnoApp/static/js/runtime-main.fc1a18b7.js"
+  },
+  {
+    "revision": "48dab3905c327aea0b0b56ba5c1a5f9a",
+    "url": "/NumoUnoApp/static/media/active.48dab390.svg"
+  },
+  {
+    "revision": "b87d7803043ccac2504d8ed57315f186",
+    "url": "/NumoUnoApp/static/media/arrow.b87d7803.svg"
+  },
+  {
+    "revision": "80fa7da96ee0136ad392fb87f6046adf",
+    "url": "/NumoUnoApp/static/media/arrowdown.80fa7da9.svg"
+  },
+  {
+    "revision": "4cbe3c342c65b3c4adb457f65fda365e",
+    "url": "/NumoUnoApp/static/media/circle.4cbe3c34.svg"
+  },
+  {
+    "revision": "675ccde152dd911ae9e21f0c7a83f034",
+    "url": "/NumoUnoApp/static/media/company.675ccde1.svg"
+  },
+  {
+    "revision": "0c22c608c7450549f4e7d4d9c80021f9",
+    "url": "/NumoUnoApp/static/media/connections.0c22c608.svg"
+  },
+  {
+    "revision": "66ae8336a39e2e521e4b0a4bc34a8ece",
+    "url": "/NumoUnoApp/static/media/contact.66ae8336.svg"
+  },
+  {
+    "revision": "ca83123ddaceb572587476cae3003dea",
+    "url": "/NumoUnoApp/static/media/dual.ca83123d.svg"
+  },
+  {
+    "revision": "8163a8b6ff9611a29477ac425ef947c0",
+    "url": "/NumoUnoApp/static/media/hamberger.8163a8b6.svg"
+  },
+  {
+    "revision": "3655b38d3c3a2c8dd24e99cf206eea65",
+    "url": "/NumoUnoApp/static/media/message.3655b38d.svg"
+  },
+  {
+    "revision": "483b7c65d4cdf134cedd2b52b4836e2f",
+    "url": "/NumoUnoApp/static/media/peers.483b7c65.svg"
+  },
+  {
+    "revision": "de65546fcab7801d812240946c453079",
+    "url": "/NumoUnoApp/static/media/pointarrow.de65546f.svg"
+  },
+  {
+    "revision": "08ec164eedd7d85850ddc606952c154d",
+    "url": "/NumoUnoApp/static/media/popularTime.08ec164e.svg"
+  },
+  {
+    "revision": "ef67753d6d2adf49845dfad80a2ba800",
+    "url": "/NumoUnoApp/static/media/popularsector2.ef67753d.svg"
+  },
+  {
+    "revision": "8d588c6257a2d233ff27cb79b13215db",
+    "url": "/NumoUnoApp/static/media/preferredSector.8d588c62.svg"
+  },
+  {
+    "revision": "50b777fdcbbd4fd8ccc1c1864b2f4a09",
+    "url": "/NumoUnoApp/static/media/profile.50b777fd.svg"
+  },
+  {
+    "revision": "749ff5103242297155283ecf57c2392e",
+    "url": "/NumoUnoApp/static/media/score.749ff510.svg"
+  },
+  {
+    "revision": "f4f0772648b1bdd743092c3ae8639b1d",
+    "url": "/NumoUnoApp/static/media/snapshot.f4f07726.svg"
+  },
+  {
+    "revision": "660e8a751740ffa46d2f7b32dba54a9f",
+    "url": "/NumoUnoApp/static/media/time.660e8a75.svg"
+  },
+  {
+    "revision": "a360ec6539f69c1804721e14675f1c75",
+    "url": "/NumoUnoApp/static/media/visits.a360ec65.svg"
+  }
+]);
